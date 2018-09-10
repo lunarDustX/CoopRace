c0288fe68b61b56439af27e001fb9427578f8b0a
lunarDustX <365877793@qq.com>
2018-09-10 20:15:26 +0800

    Sep.10th
    1. add dash
    2. add pillar/ajust level

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -6,17 +6,25 @@ public class Player : MonoBehaviour {
 
     private Vector3 moveDir;
     private Rigidbody rb;
-    public float speed;
+    public float moveSpeed;
 
-    //public int gold = 1;
     public GameObject b;
     public GameObject p;
+    public GameObject DashEffectPrefab;
 
     public int playerNumber;
 
-    private bool onGround;
     public int MAX_JUMP = 2;
-    private int currentJumps;
+    public int MAX_DASH = 2;
+   
+    private bool onGround;
+    private int currentJump;
+    private int currentDash;
+    private bool jump = false;
+
+    private float dashTime;
+    public float startDashTime;
+    public float dashSpeed;
 
     public LayerMask groundLayer;
 
@@ -25,14 +33,18 @@ public class Player : MonoBehaviour {
     private string jumpButton;
 
     private float distToGround;
-    private float poleLength = 1f;
 
+    private float poleLength = 1f;
+    private float deathPenaltyLength = 0.15f;
+    private float minLength = 0.4f;
+
+    private Transform spawnPoint;
 
 	// Use this for initialization
 	void Start () 
     {
-
         //distToGround = GetComponent<BoxCollider>().bounds.extents.y;
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
 
         rb = GetComponent<Rigidbody>();
 
@@ -40,13 +52,83 @@ public class Player : MonoBehaviour {
         V_axisName = "Vertical" + playerNumber;
         jumpButton = "Jump" + playerNumber;
 
+        dashTime = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        float h = Input.GetAxisRaw(H_axisName);
-        float v = Input.GetAxisRaw(V_axisName);
+        //float h = Input.GetAxisRaw(H_axisName);
+        //float v = Input.GetAxisRaw(V_axisName);
+
+        float h = 0, v = 0;
+        GamePadState state;
+        jump = false;
+
+        switch (playerNumber) {
+            case 1: 
+                h = GamePad.GetAxis(CAxis.LX, PlayerIndex.One);
+                v = -GamePad.GetAxis(CAxis.LY, PlayerIndex.One);
+                state = GamePad.GetState(PlayerIndex.One);
+                if (state.Pressed(CButton.A)) jump = true;
+
+                if (currentDash < MAX_DASH)
+                {
+                    if (state.Pressed(CButton.B)) 
+                    { 
+                        dashTime = startDashTime; 
+                        currentDash++; 
+                        Instantiate(DashEffectPrefab, transform.position, Quaternion.identity);
+                    }
+                }
+                break;
+
+            case 2:
+                h = GamePad.GetAxis(CAxis.LX, PlayerIndex.Two);
+                v = -GamePad.GetAxis(CAxis.LY, PlayerIndex.Two);
+                state = GamePad.GetState(PlayerIndex.Two);
+                if (state.Pressed(CButton.A)) jump = true;
+                if (currentDash < MAX_DASH)
+                {
+                    if (state.Pressed(CButton.B))
+                    {
+                        dashTime = startDashTime;
+                        currentDash++;
+                        Instantiate(DashEffectPrefab, transform.position, Quaternion.identity);
+                    }
+                }
+                break;
+            case 3:
+                h = GamePad.GetAxis(CAxis.LX, PlayerIndex.Three);
+                v = -GamePad.GetAxis(CAxis.LY, PlayerIndex.Three);
+                state = GamePad.GetState(PlayerIndex.Three);
+                if (state.Pressed(CButton.A)) jump = true;
+                if (currentDash < MAX_DASH)
+                {
+                    if (state.Pressed(CButton.B))
+                    {
+                        dashTime = startDashTime;
+                        currentDash++;
+                        Instantiate(DashEffectPrefab, transform.position, Quaternion.identity);
+                    }
+                }
+                break;
+            case 4:
+                h = GamePad.GetAxis(CAxis.LX, PlayerIndex.Four);
+                v = -GamePad.GetAxis(CAxis.LY, PlayerIndex.Four);
+                state = GamePad.GetState(PlayerIndex.Four);
+                if (state.Pressed(CButton.A)) jump = true;
+                if (currentDash < MAX_DASH)
+                {
+                    if (state.Pressed(CButton.B))
+                    {
+                        dashTime = startDashTime;
+                        currentDash++;
+                        Instantiate(DashEffectPrefab, transform.position, Quaternion.identity);
+                    }
+                }
+                break;
+        }
         moveDir = new Vector3(h, 0, v).normalized;
 
         // REBORN
@@ -57,13 +139,32 @@ public class Player : MonoBehaviour {
 
 	private void FixedUpdate()
 	{
-        rb.MovePosition(transform.position + moveDir * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown(jumpButton) && (onGround || currentJumps < MAX_JUMP)) {
-            rb.velocity = new Vector3(0, 6f, 0);
-            currentJumps++;
-            onGround = false;
+        // MOVE
+        if (moveDir.magnitude > 0)
+        {
+            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
         }
+
+        // JUMP
+        if (jump) {   //if (Input.GetButtonDown(jumpButton)) {
+            if (onGround || currentJump < MAX_JUMP) 
+            {
+                rb.velocity = new Vector3(0, 6f, 0);
+                currentJump++;
+                onGround = false;
+                jump = false;
+            }
+        }
+
+        // DASH
+        if (dashTime > 0) {
+            dashTime -= Time.deltaTime;
+            if (dashTime <= 0) {
+                rb.velocity = Vector3.zero;
+            } else {
+                rb.velocity = transform.up * dashSpeed; 
+            }
+        } 
 	}
 
 	private void OnCollisionEnter(Collision collision)
@@ -71,15 +172,17 @@ public class Player : MonoBehaviour {
         if (collision.collider.tag == "GROUND")
         {
             onGround = true;
-            currentJumps = 0;
+            currentJump = 0;
+            currentDash = 0;
         }
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-        if (other.tag == "GOLD") {
-            //gold++;
+        if (other.tag == "GOLD") 
+        {
             Destroy(other.gameObject);
+            // Score Effect
 
             ScoreBoard.Instance.UpdateScore(playerNumber);
             //float s = Mathf.Sqrt(gold) * 5;
@@ -88,12 +191,11 @@ public class Player : MonoBehaviour {
 	}
 
     void Reborn() {
-        transform.position = new Vector3(0, 18f, 0);
+        transform.position = spawnPoint.position;
         rb.velocity = Vector3.zero;
 
-        poleLength -= 0.2f;
-        Debug.Log(poleLength);
-        poleLength = Mathf.Max(0.2f, poleLength);
+        poleLength -= deathPenaltyLength;
+        poleLength = Mathf.Max(minLength, poleLength);
         p.transform.localScale = new Vector3(1, poleLength, 1);
     }
 
---
+++ Assets/test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public GameObject DashEffectPrefab;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnDashEffect", .5f, 2f);
+    }
+
+    void SpawnDashEffect()
+    {
+        Instantiate(DashEffectPrefab, new Vector3(3, 3, 3), Quaternion.identity);
+    }
+}
